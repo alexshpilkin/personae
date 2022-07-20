95671dd14ee9c85f78da4bc0935c4c36c3a2f816
Alexander Shpilkin <ashpilkin@gmail.com>
2022-07-21 02:10:48 +0300

    Install bindfs

--- users/alex@etranger.nix
+++ users/alex@etranger.nix
@@ -5,6 +5,7 @@
 	imports = [ gnome ];
 
 	home.packages = with pkgs; [
+		bindfs # administration
 		pkgs-unfree.android-studio android-tools cambalache git-remote-gcrypt gnome-builder # development
 	];
 }
