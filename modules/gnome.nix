3469a85189dc601a0e1d7f8f3f35bd43580412af
Alexander Shpilkin <ashpilkin@gmail.com>
2022-10-13 19:57:43 +0300

    Install parlatype

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -15,7 +15,7 @@
 		gparted pika-backup # administration
 		evolution fractal gnome.polari transmission-gtk # communication
 		gnome.ghex gitg meld # development
-		celluloid easyeffects gnome.gnome-sound-recorder kooha lollypop pitivi # multimedia
+		celluloid easyeffects gnome.gnome-sound-recorder kooha lollypop parlatype pitivi # multimedia
 		gnome.dconf-editor gnome.gnome-shell-extensions gnome.gnome-tweaks # settings
 		hibernate-status-button syncthing-indicator # shell
 		foliate # text
