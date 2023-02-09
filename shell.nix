21dc87da978e161cde6b2a0f424271338af3abf1
Alexander Shpilkin <ashpilkin@gmail.com>
2023-02-09 20:29:00 +0200

    Add jq to development shell

--- shell.nix
+++ shell.nix
@@ -4,6 +4,7 @@
 , callPackage ? pkgs.callPackage
 , home-manager ? callPackage (import <home-manager/home-manager>) { }
 , git ? pkgs.git
+, jq ? pkgs.jq
 }:
 
-pkgs.mkShellNoCC { packages = [ git home-manager ]; }
+pkgs.mkShellNoCC { packages = [ git home-manager jq ]; }
