584ac2b569466f0ac7cf25f0da3953e4e55c5810
Alexander Shpilkin <ashpilkin@gmail.com>
2023-05-06 21:51:33 +0300

    Install expect

--- users/alex.nix
+++ users/alex.nix
@@ -278,7 +278,7 @@ in {
 		bench binutils-unwrapped.info binutils-unwrapped.man breezy cvs cvsps fossil gcc-unwrapped.info gcc-unwrapped.man git-absorb git-annex git-annex-utils hyperfine linuxPackages_latest.perf loc mercurial perf-tools radare2 tig # development
 		tealdeer cht-sh # documentation
 		binwalk dos2unix file ffmpeg imagemagick libarchive pdftk unrar-wrapper zip # formats
-		jq httpie maxima moreutils octave pup pv rlwrap simple-http-server wget xmlstarlet yt-dlp # scripting
+		expect jq httpie maxima moreutils octave pup pv rlwrap simple-http-server wget xmlstarlet yt-dlp # scripting
 		fd ripgrep ripgrep-all # search
 		colordiff editorconfig-core-c wdiff # text
 	];
