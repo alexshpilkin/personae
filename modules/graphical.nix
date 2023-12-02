1cd2ad14f469a8778596cf30b255cb7551b6e741
Alexander Shpilkin <ashpilkin@gmail.com>
2023-12-02 12:48:20 +0200

    Install Monaspace and MPLUS

--- modules/graphical.nix
+++ modules/graphical.nix
@@ -3,7 +3,7 @@
 {
 	home.packages = with pkgs; [
 		signal-desktop tdesktop # communication
-		gentium gentium-book-basic ibm-plex inconsolata iosevka paratype-pt-sans roboto # fonts
+		gentium gentium-book-basic ibm-plex inconsolata iosevka monaspace mplus-outline-fonts.githubRelease paratype-pt-sans roboto # fonts
 		darktable # multimedia
 		wl-clipboard # scripting
 		kiwix libreoffice # text
