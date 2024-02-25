515009e2577d84dd303fde0cbab4a527ca368e74
Alexander Shpilkin <ashpilkin@gmail.com>
2024-02-26 00:28:47 +0200

    Update lockfile to 2024-02-25

--- modules/graphical.nix
+++ modules/graphical.nix
@@ -6,7 +6,8 @@
 		gentium gentium-book-basic ibm-plex inconsolata iosevka monaspace mplus-outline-fonts.githubRelease paratype-pt-sans roboto # fonts
 		darktable # multimedia
 		wl-clipboard # scripting
-		kiwix libreoffice # text
+		# FIXME kiwix
+		libreoffice # text
 	];
 
 	programs.tmux.extraConfig = ''
