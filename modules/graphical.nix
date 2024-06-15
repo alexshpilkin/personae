c17f63582d5746ecb08e71878257a7608e521e79
Alexander Shpilkin <ashpilkin@gmail.com>
2024-06-15 14:17:59 +0300

    Install hotspot

--- modules/graphical.nix
+++ modules/graphical.nix
@@ -3,6 +3,7 @@
 {
 	home.packages = with pkgs; [
 		signal-desktop tdesktop # communication
+		hotspot # development
 		gentium gentium-book-basic ibm-plex inconsolata iosevka monaspace mplus-outline-fonts.githubRelease paratype-pt-sans roboto # fonts
 		darktable # multimedia
 		wl-clipboard # scripting
